b26c16c7a65f7a1b9c7b7fc31ce0fe81d8db570e
sazonov-src <sazonov89vit@gmail.com>
2023-10-17 15:25:38 +0300

    change get_order func to OrderData obj

--- domein/order/order.py
+++ domein/order/order.py
@@ -1,11 +1,12 @@
 import copy
 from collections import abc
+from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
+from domein import order
 
 from domein.order import base
 from domein.order.order_line import OrderLine
-from domein.order.payment import Payment
 from domein.order.user_data import UserData
 
 
@@ -53,27 +54,40 @@ class OrderDone(OrderNotChangeable):
     status = Status.DONE
 
 
-def get_order(
-        items: Optional[abc.Iterable[OrderLine]] = None, 
-        user_data: Optional[UserData] = None, 
-        payed: bool = False, 
-        ready: bool = False, 
-        done: bool = False):
-    l = copy.copy(locals())
-    key = "items"
-    while l:
-        key, value = l.popitem()
-        if value:
-            l.update({key: value})
-            break
-    for value in l.values():
-        if not value:
-            raise OrderDataError()
-    return {
-        "items": OrderNew,
-        "user_data": OrderCheckout,
-        "payed": OrderPayed,
-        "ready": OrderReady,
-        "done": OrderDone
-    }[key](items, user_data)
+@dataclass
+class OrderData[T: base.OrderItemsBase]:
+    items: Optional[abc.Iterable[OrderLine]] = None
+    user_data: Optional[UserData] = None
+    payed: bool = False
+    ready: bool = False
+    done: bool = False
+        
+    def __post_init__(self):
+        self._data = copy.copy(self.__dict__)
+        while self._data:
+            key, value = self._data.popitem()
+            if value:
+                self._data.update({key: value})
+                break
+        self.validate()
 
+    def validate(self):
+        for value in self._data.values():
+            if not value:
+                raise OrderDataError
+
+    def _last_attr(self):
+        try:
+            return tuple(self._data)[-1]
+        except IndexError:
+            return "items"
+
+    def get_order(self) -> T:
+        return {
+            "items": OrderNew,
+            "user_data": OrderCheckout,
+            "payed": OrderPayed,
+            "ready": OrderReady,
+            "done": OrderDone
+        }[self._last_attr()](self.items, self.user_data)
+        
--- domein/tests/tests_order/test_get_order.py
+++ domein/tests/tests_order/test_get_order.py
@@ -2,21 +2,25 @@ import pytest
 from domein import order 
 
 def test_get_order_not_data():
-    obj = order.get_order()
+    order_data = order.OrderData()
+    obj = order_data.get_order()
     assert isinstance(obj, order.OrderNew)
 
 def test_get_order_new(order_line):
-    obj = order.get_order([order_line])
+    order_data = order.OrderData([order_line])
+    obj = order_data.get_order()
     assert isinstance(obj, order.OrderNew)
 
 def test_get_order_checkout(order_line, user_data):
-    obj = order.get_order([order_line], user_data)
+    order_data = order.OrderData([order_line], user_data)
+    obj = order_data.get_order()
     assert isinstance(obj, order.OrderCheckout)
 
 def test_get_order_payed(order_line, user_data):
-    obj = order.get_order([order_line], user_data, payed=True)
+    order_data = order.OrderData([order_line], user_data, payed=True)
+    obj = order_data.get_order()
     assert isinstance(obj, order.OrderPayed)
 
 def test_get_order_error(order_line):
     with pytest.raises(order.OrderDataError):
-        order.get_order([order_line], payed=True)
+        order.OrderData([order_line], payed=True)
