85bb7ebb20689fffc32afb8098e375ddbc3093c9
sazonov-src <sazonov89vit@gmail.com>
2023-11-02 21:02:10 +0200

    add total_price to OrderItem

--- src/domain/order/order_line.py
+++ src/domain/order/order_line.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
-
+from items import Item
 
 @dataclass(frozen=True)
 class OrderLine:
-    item: int
+    item: Item
     count: int
+
+    @property
+    def total_price(self):
+        return self.item.price * self.count
