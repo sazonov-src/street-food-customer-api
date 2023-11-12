9682120c157ae516b41049de69010c4070342235
sazonov-src <sazonov89vit@gmail.com>
2023-11-12 14:15:08 +0200

    change structure order end order state objects, change tests

--- src/domain/order/orders.py
+++ src/domain/order/orders.py
@@ -5,58 +5,132 @@ import order
 
 
 class BaseOrder(ABC):
-    start_state: type[BaseStateOrder]
 
-    def __init__(
-            self, 
-            cart_lines: Iterable[order.LINE] | None = None, 
-            user_data: order.UserData | None = None, 
-            payment: order.Payment | None = None):
-        self._cart_lines = cart_lines
-        self._user_data = user_data
-        self._payment = payment
-        self._state = self.start_state(self).get_valide_state()
+    @property
+    @abstractmethod
+    def cart(self) -> order.BaseCartView:
+        pass
 
-    def update_state(self):
-        self._state = self._state.get_valide_state()
+    @property
+    @abstractmethod
+    def state(self) -> BaseState:
+        pass
 
 
-class BaseStateOrder(ABC):
+class BaseOrderCheckout(ABC):
 
-    def __init__(self, order: BaseOrder) -> None:
+    @property
+    @abstractmethod
+    def user_data(self) -> order.UserData:
+        pass
+
+    @abstractmethod
+    def mark_as_checkout(self, checkout: order.UserData):
+        pass
+
+
+class BaseOrderPayed(ABC):
+
+    @property
+    @abstractmethod
+    def payment(self) -> order.BasePayment:
+        pass
+
+    @abstractmethod
+    def mark_as_pay(self, payment: order.BasePayment):
+        pass
+
+
+class BaseCustomerOrder(BaseOrder, BaseOrderCheckout, BaseOrderPayed):
+    pass
+
+
+class BaseState(ABC):
+    cart_type: type[order.BaseCartView]
+
+
+class BaseCheckoutState(ABC):
+    @abstractmethod
+    def mark_as_checkout(self, checkout: order.UserData):
+        pass
+
+class BasePaymentState(ABC):
+    @abstractmethod
+    def mark_as_pay(self, payment: order.BasePayment):
+        pass
+
+
+class BaseCustomerState(BaseState, BaseCheckoutState, BasePaymentState):
+    def __init__(self, order) -> None:
         self._order = order
 
-    @abstractmethod
-    def get_valide_state(self) -> BaseStateOrder:
-        pass
 
-    @abstractmethod
-    def to_checkout(self, checkout):
-        pass
+class StateCustomerPayed(BaseCustomerState):
+    cart_type = order.CartNotNutable
+    def mark_as_checkout(self, checkout):
+        raise ValueError
 
-
-class StateCheckoutOrder(BaseStateOrder):
-    def get_valide_state(self):
-        return self
-
-    def to_checkout(self, checkout):
-        raise ValueError("Already checkouted")
+    def mark_as_pay(self, payment: order.BasePayment):
+        raise ValueError
 
 
-class StateNewOrder(BaseStateOrder):
-    def get_valide_state(self):
-        if self._order._user_data:
-            return StateCheckoutOrder(self._order).get_valide_state()
-        return self
+class StateCustomerCheckout(BaseCustomerState):
+    cart_type = order.CartMutable
+    def mark_as_checkout(self, checkout):
+        raise ValueError
 
-    def to_checkout(self, checkout):
+    def mark_as_pay(self, payment: order.BasePayment):
+        if payment.is_payment():
+            self._order._state = StateCustomerPayed(self._order)
+
+
+class StateCustomerNew(BaseCustomerState):
+    cart_type = order.CartMutable
+    def mark_as_checkout(self, checkout):
         self._order._user_data = checkout
+        self._order._state = StateCustomerCheckout(self._order)
+
+    def mark_as_pay(self, payment: order.BasePayment):
+        raise ValueError
 
 
-class OrderCheckout(BaseOrder):
-    start_state = StateNewOrder
+class CustomerOrder(BaseCustomerOrder):
+    def __init__(self, order_lines: Iterable[order.LINE] | None = None):
+        self._user_data: order.UserData | None = None
+        self._payment: order.BasePayment | None = None
+        self._state = StateCustomerNew(self)
+        self._cart = self.state.cart_type(*order_lines if order_lines else ())
 
-    def to_checkout(self, checkout: order.UserData):
-        self._state.to_checkout(checkout)
-        self.update_state()
+    def _reset_cart(self):
+        cart_data = self._cart.lines
+        self._cart = self._state.cart_type(*cart_data)
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def cart(self):
+        return self._cart
+
+    @property
+    def user_data(self) -> order.UserData:
+        if not self._user_data:
+            raise ValueError
+        return self._user_data
+
+    @property
+    def payment(self) -> order.BasePayment:
+        if not self._payment:
+            raise ValueError
+        return self._payment
+
+    def mark_as_checkout(self, checkout: order.UserData):
+        self._state.mark_as_checkout(checkout)
+        self._reset_cart()
+
+    def mark_as_pay(self, payment: order.BasePayment):
+        self._state.mark_as_pay(payment)
+        self._reset_cart()
+
 
--- src/domain/tests/tests_order/test_orders.py
+++ src/domain/tests/tests_order/test_orders.py
@@ -1,14 +1,21 @@
 import order
+import pytest
 
 checkout = order.UserData("Vasia", "01")
 
 def test_checkout_state_ok():
-    ord = order.OrderCheckout([],) 
-    ord.to_checkout(checkout)
-    assert isinstance(ord._state, order.StateCheckoutOrder)
+    ord = order.CustomerOrder([],) 
+    ord.mark_as_checkout(checkout)
+    assert isinstance(ord.state, order.StateCustomerCheckout)
+    assert isinstance(ord.cart, order.CartMutable)
+    assert isinstance(ord.user_data, order.UserData)
 
 def test_new_state_ok():
-    ord = order.OrderCheckout([],) 
-    assert isinstance(ord._state, order.StateNewOrder)
+    ord = order.CustomerOrder([],) 
+    assert isinstance(ord.state, order.StateCustomerNew)
+    assert isinstance(ord.cart, order.CartMutable)
+    with pytest.raises(ValueError):
+        ord.user_data
+
 
 
