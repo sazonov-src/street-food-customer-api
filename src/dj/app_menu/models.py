576704a1bc0d678c43813a024138b3cca4cf2375
sazonov-src <sazonov89vit@gmail.com>
2023-11-14 21:21:56 +0200

    add 'ready' and 'done' fields to 'Order' model

--- src/dj/app_menu/models.py
+++ src/dj/app_menu/models.py
@@ -5,6 +5,6 @@ class MenuItem(models.Model):
     """Розширює MenuItemGeneral більш конкретними даними"""
     title = models.CharField(max_length=255, verbose_name='Назва')
     description = models.TextField(verbose_name='Опис')
-    price = models.IntegerField(verbose_name='Ціна')
+    price = models.FloatField(verbose_name='Ціна')
 
 
